e97139f98a04d8bf822b4be16d2253c71d438fee
Ivanzhela <ivanjelad@abv.bg>
2023-08-08 18:41:57 +0300

    update jwt

--- services/authService1.js
+++ services/authService1.js
@@ -1,7 +1,7 @@
 const User = require('../models/User1');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = require('../config/env');
+const { JWT_SECRET } = require('../config/env');
 
 const blacklist = new Set();
 
