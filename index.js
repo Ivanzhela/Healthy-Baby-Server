203fb2777b4cc30d1d058db4c562441531a02402
Ivanzhela <ivanjelad@abv.bg>
2023-08-08 18:08:45 +0300

    update cors

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const router = require("./routes.js");
 
 const app = express();
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
     
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // Разрешете заявките от вашия уеб сайт
-    res.header('Access-Control-Allow-Credentials', 'true'); // Разрешете изпращането на кукита (credentials)
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-authorization');
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      return res.status(200).json({});
-    }
-    next();
-  });
-// app.use(cors());
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // Разрешете заявките от вашия уеб сайт
+//     res.header('Access-Control-Allow-Credentials', 'true'); // Разрешете изпращането на кукита (credentials)
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-authorization');
+//     if (req.method === 'OPTIONS') {
+//       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//       return res.status(200).json({});
+//     }
+//     next();
+//   });
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(auth);
