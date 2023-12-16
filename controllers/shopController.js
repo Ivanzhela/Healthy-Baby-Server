9b60b2db53b674e5b9ce3ee44a95a60cc3094846
Ivanzhela <ivanjelad@abv.bg>
2023-12-16 17:01:06 +0200

    update Shop Controller

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -32,13 +32,13 @@ router.get("/", async (req, res) => {
   }
 
   try {
-    res.status(200).json(await getAll(options));
+    res.status(200).json(await getAll({}));
   } catch (err) {
     parseError(err, res);
   }
 });
 
-router.post("/", isUser, async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const result = await create({ ...req.body });
     res.status(200).json(result);
